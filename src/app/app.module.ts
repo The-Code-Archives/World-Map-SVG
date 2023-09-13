4a5aebdf9bee49beeb1bf07291b58fe1e10e93e0
TheInfiltrator <Theinfiltrator856@gmail.com>
2023-09-13 11:32:53 -0700

    Generated files for app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WorldInfoComponent } from './components/world-info/world-info.component';
+import { WorldComponent } from './components/world/world.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorldInfoComponent,
+    WorldComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/world-info/world-info.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-world-info',
+  templateUrl: './world-info.component.html',
+  styleUrls: ['./world-info.component.scss']
+})
+export class WorldInfoComponent {
+
+}
