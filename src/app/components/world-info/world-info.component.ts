8231c2747d60e74a63a5d202d3e7820edc0c2cd7
TheInfiltrator <Theinfiltrator856@gmail.com>
2023-09-20 18:17:49 -0700

    Removed unnecessary components

--- src/app/components/world-info/world-info.component.ts
+++ src/app/components/world-info/world-info.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Input,Output, EventEmitter } from '@angular/core';
-import { WorldComponent } from '../world/world.component';
+import { Component, Input} from '@angular/core';
+
 
 @Component({
   selector: 'app-world-info',
